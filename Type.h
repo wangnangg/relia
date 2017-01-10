185073a17a1d4c22d27f92eb8b04ce1910d71780
Nan Wang <wangnangg@gmail.com>
2017-01-10 00:20:50 -0500

    add exception

--- Type.h
+++ Type.h
@@ -1,6 +1,7 @@
 #pragma once
 
-
+#include <exception>
+#include <string>
 typedef unsigned uint_t;
 typedef int int_t;
 
@@ -15,5 +16,18 @@ enum TransType
 {
     Imme = 0,
     Exp = 1
+};
+
+
+
+class Exception : public std::exception
+{
+    std::string msg;
+public:
+    Exception(std::string msg):msg(std::move(msg)) {}
+    virtual const char* what() const noexcept
+    {
+       return msg.c_str();
+    }
 };
 
